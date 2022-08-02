c1a4c2ca55ed3bc291e155638f97195bc93597f0
Suvesh <anmolmoza2@gmail.com>
2022-08-02 17:00:36 +0530

    added error page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import img from '../assests/images/not-found.svg';
+import Wrapper from '../assests/wrappers/ErrorPage';
 
 const Error = () => {
-	return <h1>Error</h1>;
+	return (
+		<Wrapper className="full-page">
+			<div>
+				<img src={img} alt="not-found" />
+				<h3>Page does not exist</h3>
+				<Link to="/">Redirect to home</Link>
+			</div>
+		</Wrapper>
+	);
 };
 
 export default Error;
